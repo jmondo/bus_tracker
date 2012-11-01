040b48f09fd8b5c3bad769ab57925257afc35897
John Gesimondo <john@jmondo.com>
2012-11-01 00:06:09 -0700

    22 both ways, merged hashes when one goes to bryant grr

--- bus.rb
+++ bus.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
 require 'json'
+require 'httparty'
+require 'active_support/core_ext/enumerable.rb'
 
 get '/' do
-  hash = {value: {inbound: [15, 34, 14], outbound: [13, 22, 34]}}
-  hash.to_json
+  content_type :json
+  {potrero: predictions(0), pacific: predictions(1)}.to_json
+end
+
+def predictions(index)
+  prediction_objects = [xml["body"]["predictions"][index]["direction"]].flatten.collect { |h| h["prediction"] }.flatten
+  prediction_objects.collect {|p| p["minutes"]}
+end
+
+def xml
+  @xml ||= HTTParty.get('http://webservices.nextbus.com/service/publicXMLFeed?command=predictionsForMultiStops&a=sf-muni&stops=22%7c4622&stops=22%7c4621&stops=21%7c4994&stops=21%7c7423')
 end
